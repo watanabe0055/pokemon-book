34f69b79b85d66f6cf9d47b3d6798e9268392cdd
watanabe0055 <mailok1212+github@gmail.com>
2024-12-26 12:23:36 +0900

    update: スクロールでポケモンを取得

---
+++ src/app/jotai/pokemon/get.ts
@@ -0,0 +1,4 @@
+import { GetPokemonDataUnionSpeciesListType } from "@/app/type/pokemon";
+import { atom } from "jotai";
+
+export const allGetPokemonAtom = atom<GetPokemonDataUnionSpeciesListType>();
---
+++ src/app/lib/scroll/index.ts
@@ -0,0 +1,29 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+function useScrollDetection() {
+  const [isBottom, setIsBottom] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.innerHeight + window.scrollY;
+      const documentHeight = document.documentElement.offsetHeight;
+
+      if (scrollPosition >= documentHeight) {
+        setIsBottom(true);
+      } else {
+        setIsBottom(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return isBottom;
+}
+
+export default useScrollDetection;
