6435d06ef002b4548fa380430aec57e9a1db4694
watanabe0055 <mailok1212+github@gmail.com>
2025-01-15 20:25:19 +0900

    fix* 2type filter

--- src/app/components/typeFilter/useModel.ts
+++ src/app/components/typeFilter/useModel.ts
@@ -1,32 +1,62 @@
 import { typeListPokemonAtom } from "@/app/jotai/pokemon/get";
 import { selectedTypePokemonListAtom } from "@/app/jotai/pokemon/reset";
 import { fetchPokemonData, fetchPokemonDataByType } from "@/app/lib/fetch";
-import { ConvertPokemonUnionSpeciesType } from "@/app/type/pokemon";
+import { ConvertPokemonUnionSpeciesType, TypeName } from "@/app/type/pokemon";
+import { AbilityListHonoResponseType } from "@/app/type/pokemonAbility";
 import { ResultsType } from "@/app/type/type";
-import { useAtom, useSetAtom } from "jotai";
+import { useAtom, useAtomValue, useSetAtom } from "jotai";
 import { useResetAtom } from "jotai/utils";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 type pokemonTypesProps = {
   typeList: ResultsType[];
 };
 
 const useModel = ({ typeList }: pokemonTypesProps) => {
-  const [selectedTypes, setSelectedTypes] = useState<string[]>([]);
+  const [selectedTypes, setSelectedTypes] = useState<TypeName[]>([]);
 
   const [typeListPokemon, setTypeListPokemonAtom] =
     useAtom(typeListPokemonAtom);
 
-  const setSelectedTypePokemonList = useSetAtom(selectedTypePokemonListAtom);
+  const [selectedTypePokemonList, setSelectedTypePokemonList] = useAtom(
+    selectedTypePokemonListAtom
+  );
   const resetPokemonList = useResetAtom(selectedTypePokemonListAtom);
 
-  const handleTypeChange = (typeName: string) => {
+  // waterだけも服見たい時はこっち
+  // const filteredPokemonList = useMemo(() => {
+  //   return selectedTypePokemonList.filter((poke) => {
+  //     // poke.types が selectedTypes のみ含む場合にフィルター
+  //     const typeNames = poke.types.map((type) => type.type.name);
+  //     return typeNames.every((typeName) => selectedTypes.includes(typeName));
+  //   });
+  // }, [selectedTypePokemonList, selectedTypes]);
+
+  const filteredPokemonList = useMemo(() => {
+    return selectedTypePokemonList.filter((poke) => {
+      const typeNames = poke.types.map((type) => type.type.name);
+
+      // selectedTypes と完全一致するかチェック
+      const hasAllSelectedTypes =
+        typeNames.length === selectedTypes.length && // タイプ数が一致
+        selectedTypes.every((type) => typeNames.includes(type)); // 全て含む
+
+      return hasAllSelectedTypes;
+    });
+  }, [selectedTypePokemonList, selectedTypes]);
+
+  // useEffect(() => {
+  //   setSelectedTypePokemonList(filteredPokemonList);
+  // }, [selectedTypes[1]]);
+
+  const handleTypeChange = (typeName: TypeName) => {
     setSelectedTypes((prev) =>
       prev.includes(typeName)
         ? prev.filter((type) => type !== typeName)
         : [...prev, typeName]
     );
   };
+
   const omitTheTypeWithNoIcons = typeList.filter(
     (type) => type.name !== "stellar" && type.name !== "unknown"
   );
@@ -64,7 +94,10 @@ const useModel = ({ typeList }: pokemonTypesProps) => {
       const pokemonDataWithoutUndefined = results
         .map((result) => result.pokemonData)
         .filter(
-          (data): data is ConvertPokemonUnionSpeciesType => data !== undefined
+          (
+            data
+          ): data is ConvertPokemonUnionSpeciesType &
+            AbilityListHonoResponseType => data !== undefined
         );
 
       // 既存のリストに追加
