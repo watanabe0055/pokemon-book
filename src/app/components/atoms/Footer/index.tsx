5b1c4035ce707cb203ba719598a4e788fd37ca28
watanabe0055 <mailok1212+github@gmail.com>
2025-01-20 23:26:30 +0900

    fix

--- src/app/components/atoms/Footer/index.tsx
+++ src/app/components/atoms/Footer/index.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { Github, Twitter } from "lucide-react";
+import { Github } from "lucide-react";
 
 export default function Footer() {
   return (
@@ -8,29 +8,17 @@ export default function Footer() {
         <div className="flex flex-col items-center justify-between md:flex-row">
           <div className="mb-4 md:mb-0">
             <h3 className="text-xl font-bold text-indigo-600">PokeSearch</h3>
-            <p className="text-sm">Your ultimate Pokémon companion</p>
+            <p className="text-sm">Your Pokémon companion</p>
           </div>
           <nav className="flex flex-wrap justify-center mb-4 space-x-6 md:justify-end md:mb-0">
             <Link href="/" className="transition-colors hover:text-indigo-600">
-              Home
+              Pokémon List
             </Link>
             <Link
               href="/search"
               className="transition-colors hover:text-indigo-600"
             >
               Search
-            </Link>
-            <Link
-              href="/list"
-              className="transition-colors hover:text-indigo-600"
-            >
-              Pokémon List
-            </Link>
-            <Link
-              href="/about"
-              className="transition-colors hover:text-indigo-600"
-            >
-              About
             </Link>
           </nav>
           <div className="flex space-x-4">
@@ -41,14 +29,6 @@ export default function Footer() {
               className="text-gray-400 transition-colors hover:text-indigo-600"
             >
               <Github className="w-5 h-5" />
-            </a>
-            <a
-              href="https://twitter.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-gray-400 transition-colors hover:text-indigo-600"
-            >
-              <Twitter className="w-5 h-5" />
             </a>
           </div>
         </div>
