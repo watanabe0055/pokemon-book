8f8b8608f699737be7adbb7a7e00ac5fcde2c7c1
watanabe0055 <mailok1212+github@gmail.com>
2025-01-27 15:42:52 +0900

    build: error fix vlo2

--- src/app/(Page)/pokemon/[slug]/page.tsx
+++ src/app/(Page)/pokemon/[slug]/page.tsx
@@ -1,9 +1,13 @@
 import PokemonSearchDetail from "@/app/components/ModalContainer/pokemonSearchDetail";
 import { fetchPokemonData } from "@/app/lib/fetch";
 
-async function getServerPokemonData(params: { slug: string }) {
+type Props = {
+  params: Promise<{ slug: string }>;
+};
+
+async function getServerPokemonData(slug: string) {
   try {
-    const pokemon = await fetchPokemonData({ id: params.slug });
+    const pokemon = await fetchPokemonData({ id: slug });
     if (!pokemon?.pokemonData) {
       throw new Error("Pokemon not found");
     }
@@ -14,13 +18,10 @@ async function getServerPokemonData(params: { slug: string }) {
   }
 }
 
-export default async function Article({
-  params,
-}: {
-  params: { slug: string };
-}) {
+export default async function Article({ params }: Props) {
+  const { slug } = await params;
   // getData を呼び出してデータを取得
-  const { pokemon } = await getServerPokemonData(params);
+  const { pokemon } = await getServerPokemonData(slug);
 
   return (
     <main>
