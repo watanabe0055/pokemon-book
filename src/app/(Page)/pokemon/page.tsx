b05faedfc0317b1345af6dce9d66f05827967eb2
watanabe0055 <mailok1212+github@gmail.com>
2025-01-28 22:50:02 +0900

    feat: ページとfetchまで移行

--- src/app/(Page)/pokemon/page.tsx
+++ src/app/(Page)/pokemon/page.tsx
@@ -1,17 +1,12 @@
-import TypeFilter from "../../components/typeFilter";
 import { fetchAllPokemonData } from "../../lib/fetch";
-import { fetchPokemonTypes } from "../../lib/fetch/type";
 import PokemonIndexPagePart from "../../pageParts/pokemon";
 
 export default async function Home() {
-  const pokemonTypes = await fetchPokemonTypes();
   const getPokemonData = await fetchAllPokemonData({ offset: 1 });
 
   return (
-    <main className="container px-4 py-8 mx-auto">
-      <h1 className="mb-8 text-4xl font-bold text-center">Pokemon Types</h1>
-      <TypeFilter typeList={pokemonTypes} />
+    <div className="container px-4 py-8 mx-auto">
       <PokemonIndexPagePart InitialPokemonData={getPokemonData} />
-    </main>
+    </div>
   );
 }
---
+++ src/app/(Page)/pokemon/type/page.tsx
@@ -0,0 +1,13 @@
+import TypeFilter from "@/app/components/typeFilter";
+import { fetchPokemonTypes } from "@/app/lib/fetch/type";
+
+export default async function Home() {
+  const pokemonTypes = await fetchPokemonTypes();
+
+  return (
+    <div className="container px-4 py-8 mx-auto">
+      <h1 className="mb-8 text-4xl font-bold text-center">Pokemon Types</h1>
+      <TypeFilter typeList={pokemonTypes} />
+    </div>
+  );
+}
