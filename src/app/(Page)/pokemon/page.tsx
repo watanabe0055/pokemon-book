a2935c114d9cf0e59a7534952398ee0c853ed723
watanabe0055 <mailok1212+github@gmail.com>
2025-01-28 23:18:44 +0900

    fix: タイトル中ふく

--- src/app/(Page)/pokemon/page.tsx
+++ src/app/(Page)/pokemon/page.tsx
@@ -6,6 +6,7 @@ export default async function Home() {
 
   return (
     <div className="container px-4 py-8 mx-auto">
+      <h1 className="mb-8 text-4xl font-bold text-center">Poke図鑑</h1>
       <PokemonIndexPagePart InitialPokemonData={getPokemonData} />
     </div>
   );
